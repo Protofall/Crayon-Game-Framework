db59e6d4dfa6ac28abbbaaff1fb4b4b3d2797de5
David Harley <david74437@gmail.com>
2018-01-25 18:06:17 +1030

    Now fully compiles (Forgot to make main.o) and make clean now deletes the .o files too

--- Crayon/main.c
+++ Crayon/main.c
@@ -1,12 +1,15 @@
 //KOS libraries
 #include "crayon/memory.h"
 #include "crayon/debug.h"
-#include "crayon/structs.h"
 #include "crayon/draw.h"
+#include "crayon/render_structs.h"
+#include "crayon/texture_structs.h"
 
-#include <kos.h>  //Replace this with includes for the crayon libs
+//#include <kos.h>  //Replace this with includes for the crayon libs
 
 int main() {
+
+	error_freeze("Goodbye world!\n");
 
     return 0;
 }
