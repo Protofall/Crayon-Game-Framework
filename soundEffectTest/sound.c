3ec35f2e1cc0fa50926f67efb207a59ec571fdb9
Protofall <david74437@gmail.com>
2018-08-30 19:31:30 +0930

    Accidentally had two main files and was using the wrong one (In sound test). Now using right file with minimum libraries

--- soundEffectTest/sound.c
+++ soundEffectTest/sound.c
@@ -1,8 +1,8 @@
-#include <kos.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 //For the sound effects
-#include <dc/spu.h> //Might be redundant
-#include <dc/sound/sound.h> //Might be redundant
 #include <dc/sound/stream.h>
 #include <dc/sound/sfxmgr.h>
  
@@ -14,24 +14,21 @@
 extern uint8 romdisk_boot[];
 KOS_INIT_ROMDISK(romdisk_boot);
 
+//When running the program, wait a few seconds before pressing buttons since it takes a second or two to load
 int main(){
     snd_stream_init();
 
-    //Mount romdisk
-    //memory_mount_romdisk("/cd/sound.img", "/sound");
     sfxhnd_t Sound_T = snd_sfx_load("/rd/Sound_009.wav");
-    //fs_romdisk_unmount("/sound");
 
     uint32_t previous_buttons = 0;	//Records the previous buttons polled
     maple_device_t  * controller;
     cont_state_t * st;
  
     while(1){
-        //Read controller input
 	controller = maple_enum_type(0, MAPLE_FUNC_CONTROLLER);	//Reads the first plugged in controller
         st = (cont_state_t *)maple_dev_status(controller);	//State of controller
 
-        if(st->buttons & (1 << 2) && (!(previous_buttons & (1 << 2)))){	//When the button is pressed, play the sound
+        if(st->buttons & (1 << 2) && (!(previous_buttons & (1 << 2)))){	//When the "A" button is pressed, play the sound
             snd_sfx_play(Sound_T, 127, 128);    // ~50% volume, pan centred
         }
 
