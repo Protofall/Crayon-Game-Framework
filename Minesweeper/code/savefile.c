32ed366e428ffa4d2fb54c50a6b8f14a2d92c888
Protofall <david74437@gmail.com>
2018-12-12 16:44:30 +1030

    Almost finished updating the save code, but I want to break out the save struct and re-locate the savefiles to crayon...however with the ghetto app_id fix I dunno how that would work...

--- Minesweeper/code/savefile.c
+++ Minesweeper/code/savefile.c
@@ -1,53 +1,5 @@
 #include "savefile.h"
 
-void vmu_load_icon(SaveFileDetails_t * save){
-
-	file_t icon_files;
-
-	icon_files = fs_open("/Save/IMAGE.BIN", O_RDONLY);
-	save->save_file_icon = (unsigned char *) malloc(fs_total(icon_files));
-	fs_read(icon_files, save->save_file_icon, fs_total(icon_files));
-	fs_close(icon_files);
-
-	icon_files = fs_open("/Save/PALLETTE.BIN", O_RDONLY);
-	save->save_file_palette = (unsigned short *) malloc(fs_total(icon_files));
-	fs_read(icon_files, save->save_file_palette, fs_total(icon_files));
-	fs_close(icon_files);
-
-	return;
-}
-
-void vmu_free_icon(SaveFileDetails_t * save){
-
-	free(save->save_file_icon);
-	free(save->save_file_palette);
-
-	return;
-}
-
-/*
-
-port	slot	bit
-0		1		(1 << 7)
-0		2		(1 << 6)
-1		1		(1 << 5)
-1		2		(1 << 4)
-2		1		(1 << 3)
-2		2		(1 << 2)
-3		1		(1 << 1)
-3		2		(1 << 0)
-
-*/
-
-uint8_t vmu_get_bit(uint8_t valid_vmus, int8_t port, int8_t slot){
-	return !!(valid_vmus & (1 << ((2 - slot) + 6 - (2 * port))));
-}
-
-void vmu_set_bit(uint8_t * valid_vmus, int8_t port, int8_t slot){
-	*valid_vmus |= (1 << ((2 - slot) + 6 - (2 * port)));
-	return;
-}
-
 //Note this assumes the vmu chosen is valid
 uint8_t vmu_check_for_savefile(SaveFileDetails_t * save, int8_t port, int8_t slot){
 	vmu_pkg_t pkg;
@@ -77,14 +29,33 @@ uint8_t vmu_check_for_savefile(SaveFileDetails_t * save, int8_t port, int8_t slo
 
 	free(pkg_out);
 
-	//CURRENTLY THEY'RE ALL HARDCODED and don't work...
-	//The IDs don't match
-	// if(strcmp(pkg.app_id, save->save_id)){
-	// 	return 0;
-	// }
-	// if(strcmp(pkg.app_id, "Proto_Minesweeper")){
-	// 	return 0;
-	// }
+	//This uses the old broken off id and the new id
+	if(!(strcmp(pkg.app_id, save->app_id) == 0 ||
+		strcmp(pkg.app_id, "Proto_Minesweepe") == 0)){
+		return 0;
+	}
+	return 1;
+}
+
+//MAPLE_FUNC_MEMCARD
+uint8_t vmu_check_for_device(int8_t port, int8_t slot, uint32_t function){
+	maple_device_t *vmu;
+
+	//Invalid controller/port
+	if(port < 0 || port > 3 || slot < 1 || slot > 2){
+		return 0;
+	}
+
+	//Make sure there's a device in the port/slot
+	if(!(vmu = maple_enum_dev(port, slot))){
+		return 0;
+	}
+
+	//Check the device is valid and it has a certain function
+	if(!vmu->valid || !(vmu->info.functions & function)){
+		return 0;
+	}
+
 	return 1;
 }
 
@@ -101,27 +72,55 @@ uint16_t vmu_get_save_block_count(size_t savefile_size){
 	return pkg_size >> 9;	//2^9 is 512 so this gets us the number of blocks
 }
 
-//UNFINISHED
-uint8_t vmu_valid(SaveFileDetails_t * save){
+uint8_t vmu_get_bit(uint8_t vmu_bitmap, int8_t port, int8_t slot){
+	return !!(vmu_bitmap & (1 << ((2 - slot) + 6 - (2 * port))));
+}
+
+void vmu_set_bit(uint8_t * vmu_bitmap, int8_t port, int8_t slot){
+	*vmu_bitmap |= (1 << ((2 - slot) + 6 - (2 * port)));
+	return;
+}
+
+//The paths are hard coded...
+void vmu_load_icon(SaveFileDetails_t * save){
+	file_t icon_files;
+
+	icon_files = fs_open("/Save/IMAGE.BIN", O_RDONLY);
+	save->save_file_icon = (unsigned char *) malloc(fs_total(icon_files));
+	fs_read(icon_files, save->save_file_icon, fs_total(icon_files));
+	fs_close(icon_files);
+
+	icon_files = fs_open("/Save/PALLETTE.BIN", O_RDONLY);
+	save->save_file_palette = (unsigned short *) malloc(fs_total(icon_files));
+	fs_read(icon_files, save->save_file_palette, fs_total(icon_files));
+	fs_close(icon_files);
+
+	return;
+}
+
+void vmu_free_icon(SaveFileDetails_t * save){
+	free(save->save_file_icon);
+	free(save->save_file_palette);
+
+	return;
+}
+
+//UNFINISHED?
+uint8_t vmu_get_valid_vmus(SaveFileDetails_t * save){
 	maple_device_t *vmu;
 	uint8_t valid_vmus = 0;	//a1a2b1b2c1c2d1d2
 
 	int i, j;
 	for(i = 0; i <= 3; i++){
 		for(j = 1; j <= 2; j++){
-			//No device present
-			if(!(vmu = maple_enum_dev(i, j))){
-				continue;
-			}
-
-			//Not valid or a memory card
-			if(!vmu->valid || !(vmu->info.functions & MAPLE_FUNC_MEMCARD)){
+			if(!vmu_check_for_device(i, j, MAPLE_FUNC_MEMCARD)){
 				continue;
 			}
 
 			//Check if a save file DNE
 			if(!vmu_check_for_savefile(save, i, j)){
 				//Not enough free space for a savefile
+				vmu = maple_enum_dev(i, j);
 				if(vmufs_free_blocks(vmu) < vmu_get_save_block_count(save->save_file_size)){
 					continue;
 				}
@@ -135,43 +134,13 @@ uint8_t vmu_valid(SaveFileDetails_t * save){
 	return valid_vmus;
 }
 
-uint8_t vmu_valid_screens(){
-	maple_device_t *vmu;
-	uint8_t valid_screens = 0;	//a1a2b1b2c1c2d1d2
-
-	int i, j;
-	for(i = 0; i <= 3; i++){
-		for(j = 1; j <= 2; j++){
-			if(!(vmu = maple_enum_dev(i, j))){
-				continue;
-			}
-
-			if(!vmu->valid || !(vmu->info.functions & MAPLE_FUNC_LCD)){
-				continue;
-			}
-
-			//If we got here, we have a screen
-			vmu_set_bit(&valid_screens, i, j);
-		}
-	}
-
-	return valid_screens;
-}
-
 uint8_t vmu_get_savefiles(SaveFileDetails_t * save){
-	maple_device_t *vmu;
 	uint8_t valid_saves = 0;	//a1a2b1b2c1c2d1d2
 
 	int i, j;
 	for(i = 0; i <= 3; i++){
 		for(j = 1; j <= 2; j++){
-			//No device present
-			if(!(vmu = maple_enum_dev(i, j))){
-				continue;
-			}
-
-			//Not valid or a memory card
-			if(!vmu->valid || !(vmu->info.functions & MAPLE_FUNC_MEMCARD)){
+			if(!vmu_check_for_device(i, j, MAPLE_FUNC_MEMCARD)){
 				continue;
 			}
 
@@ -184,6 +153,65 @@ uint8_t vmu_get_savefiles(SaveFileDetails_t * save){
 	return valid_saves;
 }
 
+uint8_t vmu_get_valid_screens(){
+	uint8_t valid_screens = 0;	//a1a2b1b2c1c2d1d2
+
+	int i, j;
+	for(i = 0; i <= 3; i++){
+		for(j = 1; j <= 2; j++){
+			if(!vmu_check_for_device(i, j, MAPLE_FUNC_LCD)){
+				continue;
+			}
+
+			//If we got here, we have a screen
+			vmu_set_bit(&valid_screens, i, j);
+		}
+	}
+
+	return valid_screens;
+}
+
+uint8_t vmu_load_uncompressed(SaveFileDetails_t * save){
+	vmu_pkg_t pkg;
+	uint8 *pkg_out;
+	int pkg_size;
+	FILE *fp;
+	char savename[32];
+
+	//If you call everying in the right order, this is redundant
+	//But just incase you didn't, here it is
+	if(!vmu_check_for_device(save->port, save->slot, MAPLE_FUNC_MEMCARD)){
+		return 0;
+	}
+
+	//Only 20 chara allowed at max (21 if you include '\0')
+	sprintf(savename, "/vmu/%c%d/", save->port + 97, save->slot);	//port gets converted to a, b, c or d. unit is unit
+	strcat(savename, save->save_name);
+
+	//If the VMU isn't plugged in, this will fail anyways
+	if(!(fp = fopen(savename, "rb"))){
+		return 0;
+	}
+
+	fseek(fp, 0, SEEK_SET);
+	fseek(fp, 0, SEEK_END);
+	pkg_size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	pkg_out = (uint8 *)malloc(pkg_size);
+	fread(pkg_out, pkg_size, 1, fp);
+	fclose(fp);
+
+	vmu_pkg_parse(pkg_out, &pkg);
+
+	//Read the pkg data into my struct
+	memcpy(&save->save_file, pkg.data, save->save_file_size);
+
+	free(pkg_out);
+
+	return 1;
+}
+
 int vmu_save_uncompressed(SaveFileDetails_t * save){
 	vmu_pkg_t pkg;
 	uint8 *pkg_out, *data;
@@ -194,29 +222,35 @@ int vmu_save_uncompressed(SaveFileDetails_t * save){
 	int rv = 0, blocks_freed = 0;
 	file_t f;
 
+	//If you call everying in the right order, this is redundant
+	//But just incase you didn't, here it is
+	if(!vmu_check_for_device(save->port, save->slot, MAPLE_FUNC_MEMCARD)){
+		return -4;
+	}
+
 	vmu = maple_enum_dev(save->port, save->slot);
 
 	//Only 20 chara allowed at max (21 if you include '\0')
 	sprintf(savename, "/vmu/%c%d/", save->port + 97, save->slot);	//port gets converted to a, b, c or d. unit is unit
-	strcat(savename, "MINESWEEPER.s");
+	strcat(savename, save->save_name);
 
-	int filesize = sizeof(MinesweeperSaveFile_t);
+	int filesize = save->save_file_size;
 	data = (uint8_t *) malloc(filesize);
 	if(data == NULL){
 		free(data);
-		return -1;
+		return -3;
 	}
-	memcpy(data, &save->save_file, sizeof(MinesweeperSaveFile_t));	//Last param is num of bytes and sizeof returns in bytes
+	memcpy(data, &save->save_file, save->save_file_size);	//Last param is num of bytes and sizeof returns in bytes
 
-	sprintf(pkg.desc_long, "Made with Crayon by Protofall");
-	strcpy(pkg.desc_short, "Minesweeper");
-	strcpy(pkg.app_id, "Proto_Minesweeper");
+	sprintf(pkg.desc_long, save->desc_long);
+	strcpy(pkg.desc_short, save->desc_short);
+	strcpy(pkg.app_id, save->app_id);
 	pkg.icon_cnt = 1;
 	pkg.icon_anim_speed = 0;
 	memcpy(pkg.icon_pal, save->save_file_palette, 32);
 	pkg.icon_data = save->save_file_icon;
 	pkg.eyecatch_type = VMUPKG_EC_NONE;
-	pkg.data_len = sizeof(MinesweeperSaveFile_t);	//Double check this, but I think its right
+	pkg.data_len = save->save_file_size;
 	pkg.data = data;
 
 	vmu_pkg_build(&pkg, &pkg_out, &pkg_size);
@@ -238,7 +272,7 @@ int vmu_save_uncompressed(SaveFileDetails_t * save){
 	if(!(fp = fopen(savename, "wb"))){
 		free(pkg_out);
 		free(data);
-		return -1;
+		return -2;
 	}
 
 	if(fwrite(pkg_out, 1, pkg_size, fp) != (size_t)pkg_size){
@@ -251,42 +285,4 @@ int vmu_save_uncompressed(SaveFileDetails_t * save){
 	free(data);
 
 	return rv;
-}
-
-uint8_t vmu_load_uncompressed(SaveFileDetails_t * save){
-	vmu_pkg_t pkg;
-	uint8 *pkg_out;
-	int pkg_size;
-	FILE *fp;
-	char savename[32];
-	// maple_device_t *vmu;	//Used for checking if VMU is plugged in
-
-	// vmu = maple_enum_dev(save->port, save->slot);
-
-	//Only 20 chara allowed at max (21 if you include '\0')
-	sprintf(savename, "/vmu/%c%d/", save->port + 97, save->slot);	//port gets converted to a, b, c or d. unit is unit
-	strcat(savename, "MINESWEEPER.s");
-
-	//If the VMU isn't plugged in, this will fail anyways
-	if(!(fp = fopen(savename, "rb"))){
-		return 1;
-	}
-
-	fseek(fp, 0, SEEK_SET);
-	fseek(fp, 0, SEEK_END);
-	pkg_size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
-	pkg_out = (uint8 *)malloc(pkg_size);
-	fread(pkg_out, pkg_size, 1, fp);
-	fclose(fp);
-
-	vmu_pkg_parse(pkg_out, &pkg);
-
-	//Read the pkg data into my struct
-	memcpy(&save->save_file, pkg.data, sizeof(MinesweeperSaveFile_t));
-
-	free(pkg_out);
-
-	return 0;
 }
